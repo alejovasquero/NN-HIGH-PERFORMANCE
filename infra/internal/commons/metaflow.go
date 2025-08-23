6328c2e7dc54191b710ec88fecac06e50114d352
alejovasquero <alejo_704@hotmail.com>
2025-08-23 22:07:28 +0000

    change version of ui and core

--- infra/internal/commons/metaflow.go
+++ infra/internal/commons/metaflow.go
@@ -5,6 +5,6 @@ const (
 	MetaflowUIServiceName = "metadata-ui-service-v2"
 	MetaflowDBName        = "metaflow"
 	MetaflowDBUsername    = "master"
-	MetaflowMetadataImage = "netflixoss/metaflow_metadata_service:v2.4.12"
-	MetaflowStaticUIImage = "public.ecr.aws/outerbounds/metaflow_ui:v1.2.4"
+	MetaflowMetadataImage = "netflixoss/metaflow_metadata_service:v2.5.0"
+	MetaflowStaticUIImage = "public.ecr.aws/outerbounds/metaflow_ui:v1.3.14"
 )
