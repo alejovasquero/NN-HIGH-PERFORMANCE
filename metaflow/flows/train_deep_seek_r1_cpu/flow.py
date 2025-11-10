e706b565f5ee02fdb3708439c3c56fa55c06b2eb
alejovasquero <alejo_704@hotmail.com>
2025-11-09 21:08:54 -0500

    use unsloth

--- metaflow/flows/train_deep_seek_r1_cpu/flow.py
+++ metaflow/flows/train_deep_seek_r1_cpu/flow.py
@@ -1,6 +1,11 @@
+import unsloth
 from metaflow import FlowSpec, Parameter, step
-from transformers import TrainingArguments, AutoModelForSequenceClassification, Trainer, AutoTokenizer
+from transformers import TrainingArguments
 import config
+from trl import SFTTrainer
+from unsloth import FastLanguageModel
+from unsloth import is_bfloat16_supported
+
 
 class DeepSeepR1CPUTraining(FlowSpec):
     model_config = config.DeepSeekConfig()
@@ -12,22 +17,55 @@ class DeepSeepR1CPUTraining(FlowSpec):
     @step
     def prep_model(self):
         print("Loading model...")
-        self.tokenizer = AutoTokenizer.from_pretrained(self.model_config.model_name)
-        self.training_args = TrainingArguments("deep_seek_r1_cpu")
-        self.model = AutoModelForSequenceClassification.from_pretrained(pretrained_model_name_or_path=self.model_config.model_name)
-        self.next(self.train_model)
+        model, tokenizer = FastLanguageModel.from_pretrained(model_name=self.model_config.model_name)
 
-    @step
-    def train_model(self):
-        print("Starting model fine tunning...")
-        trainer = Trainer(
-            self.model,
-            self.training_args,
-            train_dataset=[],
-            eval_dataset=[],
-            processing_class=self.tokenizer,
+        model = FastLanguageModel.get_peft_model(
+            model,
+            r=16, 
+            target_modules=[
+                "q_proj",
+                "k_proj",
+                "v_proj",
+                "o_proj",
+                "gate_proj",
+                "up_proj",
+                "down_proj",
+            ],
+            lora_alpha=16,
+            lora_dropout=0, 
+            bias="none", 
+            use_gradient_checkpointing="unsloth", 
+            random_state=3407,
+            use_rslora=False, 
+            loftq_config=None,
         )
-        trainer.train()
+
+        max_seq_length = 2048 
+        trainer = SFTTrainer(
+            model = model,
+            tokenizer = tokenizer,
+            train_dataset = [],
+            dataset_text_field = "text",
+            max_seq_length = max_seq_length,
+            dataset_num_proc = 2,
+            args = TrainingArguments(
+                per_device_train_batch_size = 2,
+                gradient_accumulation_steps = 4,
+                warmup_steps = 5,
+                max_steps = 60,
+                learning_rate = 2e-4,
+                fp16 = not is_bfloat16_supported(),
+                bf16 = is_bfloat16_supported(),
+                logging_steps = 1,
+                optim = "adamw_8bit",
+                weight_decay = 0.01,
+                lr_scheduler_type = "linear",
+                seed = 3407,
+                output_dir = "outputs",
+            ),
+        )
+        trainer_stats = trainer.train()
+        print(trainer_stats)
         self.next(self.end)
 
     @step
